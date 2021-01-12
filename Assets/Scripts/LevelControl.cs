7edaada5a756fadfcd3f23452ceb440350e284a0
Rupert Dean <deanrupert@outlook.com>
2021-01-12 23:32:03 +0000

    Working game prototype I guess
    Game has mostly working double jump, changing colours and interactivity between platforms and colours, 4 levels + menu level

---
+++ Assets/Scripts/LevelControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelControl : MonoBehaviour{
+
+  public int index;
+  public string levelName;
+
+  private bool nextLevel;
+
+  private void Update(){
+      if(nextLevel){
+        if(Input.GetKeyDown(KeyCode.E)){
+          nextLevel = false;
+          SceneManager.LoadScene(levelName);
+
+          // Load via levelName
+          // SceneManager.LoadScene(levelName);
+
+          // Restart LevelControl
+          // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+      }
+    }
+
+  private void OnTriggerEnter2D(Collider2D other){
+    if(other.CompareTag("Player")){
+      nextLevel = true;
+    }
+  }
+
+  private void OnTriggerExit2D(Collider2D other){
+    if(other.CompareTag("Player")){
+      nextLevel = false;
+    }
+  }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour{
+
+  public float speed;
+  public float jumpSpeed;
+  public Animator anim;
+
+  private int jumps;
+  private Rigidbody2D rb;
+  private BoxCollider2D boxCollider2d;
+
+  [SerializeField] private LayerMask layerMask;
+
+  // Start is called before the first frame update
+  void Start(){
+      rb = GetComponent<Rigidbody2D>();
+      boxCollider2d = GetComponent<BoxCollider2D>();
+
+      jumps = 1;
+  }
+
+  void OnTriggerEnter2D(Collider2D other){
+    if(other.gameObject.tag == "BluePortal"){
+      gameObject.layer = 8;
+      layerMask = (1 << 10) | 1;
+      anim.SetInteger("Colour", 1);
+      anim.SetTrigger("ColourChange");
+    } else if(other.gameObject.tag == "PinkPortal"){
+      gameObject.layer = 9;
+      layerMask = (1 << 11) | 1;
+      anim.SetInteger("Colour", 0);
+      anim.SetTrigger("ColourChange");
+    }
+  }
+
+  private void Movement(){
+    if(Input.GetKeyDown(KeyCode.LeftArrow)){
+      anim.transform.Rotate(0, 180, 0);
+    }
+    if(Input.GetKeyUp(KeyCode.LeftArrow)){
+      anim.transform.Rotate(0, 180, 0);
+    }
+
+    if(Input.GetKey(KeyCode.LeftArrow)){
+      anim.SetFloat("Walking", speed);
+      rb.velocity = new Vector2(-speed, rb.velocity.y);
+    } else{
+      if(Input.GetKey(KeyCode.RightArrow)){
+        anim.SetFloat("Walking", speed);
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+      } else{
+        anim.SetFloat("Walking", 0);
+        rb.velocity = new Vector2(0, rb.velocity.y);
+      }
+    }
+
+  }
+
+  void isGrounded(){
+    RaycastHit2D boxCastResults;
+    boxCastResults = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, boxCollider2d.bounds.extents.y, layerMask);
+
+    if(boxCastResults.collider != null){
+      jumps = 1;
+      anim.SetBool("OnGround", true);
+    } else {
+      anim.SetBool("OnGround", false);
+      anim.SetInteger("Jump", 0);
+    }
+  }
+
+  // Update is called once per frame
+  void Update(){
+    isGrounded();
+
+    if(Input.GetKeyDown(KeyCode.Space) && jumps > 0){
+      anim.SetBool("OnGround", false);
+      anim.SetInteger("Jump", 1);
+      rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+      jumps--;
+    }
+
+
+    Movement();
+    anim.SetFloat("VerticalVelocity", rb.velocity.y);
+
+  }
+}
